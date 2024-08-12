8aef948de8d73cb52dd88ce493700455c73ae321
Sidhant Kumar <162786760+sidhant-kumar08@users.noreply.github.com>
2024-08-12 15:12:40 +0530

    Update Notes.jsx

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
-function Notes() {
+function Notes({title, desc}) {
     return (
         <>
             <div className="notes-container">
                 <div className="notes-icons">
-                    <FaEdit />
+                    <FaEdit onClick={()=>{
+                        console.log('clicked')
+                    }} />
                     <MdDelete />
 
                 </div>
-                <h1 className='notes-title'>Title</h1>
-                <p className='notes-desc'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus non voluptatibus deserunt dolorum doloribus facere? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, perspiciatis culpa iste molestias perferendis minima? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus non voluptatibus deserunt dolorum doloribus facere? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, perspiciatis culpa iste molestias perferendis minima?</p>
+                <h1 className='notes-title'>{title}</h1>
+                <p className='notes-desc'>{desc}</p>
             </div>
         </>
     )
